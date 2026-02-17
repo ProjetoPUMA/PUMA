f0d7a328a20d7aeee710f73e43cf81babb38609a
Tais Souza <tata.ssouz47@gmail.com>
2026-02-17 15:37:47 -0300

    feat: implementação do sass ao projeto

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/css/temporary.css";
+import "./styles/css/style.css";
 import "primeflex/primeflex.css";
 
 createRoot(document.getElementById("root")!).render(
