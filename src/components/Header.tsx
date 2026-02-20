9c37be58853136fc37b0228e4f419b49e141dbfa
Tais Souza <tata.ssouz47@gmail.com>
2026-02-20 12:47:44 -0300

    refactor: ajustes no css do overlay e header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,43 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { NavLink } from "react-router-dom";
 
 function Header() {
+  const today = new Date();
+
   return (
     <header className="header">
-      <div className="logo">
-        <img src="./puma header.svg" alt="" />
+      <div className="header__container">
+        <div className="logo">
+          <img src="./puma header.svg" alt="" />
+        </div>
+        <div className="title">
+          <img src="./title header.svg" alt="" />
+        </div>
+        <div className="date">
+          <span>Data:</span>
+          <h2>{format(today, "dd/MM", { locale: ptBR })}</h2>
+        </div>
       </div>
-      <div className="title">
-        <img src="./title header.svg" alt="" />
-      </div>
-      <div className="Date">
-        Data:
-      </div>
+      <h2 className="header__name">
+        PLATAFORMA UNIFICADA DE MATERIAIS ACADÊMICOS
+      </h2>
+      <nav className="header__nav">
+        <ul>
+          <li>
+            <NavLink to="/PUMA">Mural</NavLink>
+          </li>
+          <li>
+            <NavLink to="/PUMA/materias">Matérias</NavLink>
+          </li>
+          <li>
+            <NavLink to="/PUMA/calendarios">Horários e Calendário</NavLink>
+          </li>
+          <li>
+            <NavLink to="/PUMA/documentos">Documentos</NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
