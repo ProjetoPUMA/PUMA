053217ed76f32ee8b6940e867d627aaf7262d083
Tais Souza <tata.ssouz47@gmail.com>
2026-02-22 17:00:32 -0300

    stlye: estilização das matérias

--- src/components/Subjects.tsx
+++ src/components/Subjects.tsx
@@ -20,31 +20,35 @@ function Subjects({
   const new_ID = String(id);
 
   return (
-    <li className={`schedule__container--${new_ID}`}>
-      <div className="subjects__header flex flex-column gap-1">
-        <div className="flex w-100 justify-content-between">
-          <h4>{teacher}</h4>
-          <h4>
-            {days[0]} {days.length > 1 ? `| ${days[1]}` : ""}
-          </h4>
+    <li className={`subjects__container subjects__container--${new_ID}`}>
+      <div className="subjects__content">
+        <div className="subjects__header flex flex-column gap-1">
+          <div className="flex w-100 justify-content-between subjects__info">
+            <h4>{teacher}</h4>
+            <h4>
+              {days[0]} {days.length > 1 ? `| ${days[1]}` : ""}
+            </h4>
+          </div>
+          <h2>{subject}</h2>
         </div>
-        <h2>{subject}</h2>
-      </div>
-      <div className="subjects__desc">
-        <div
-          className={`flex justify-content-between align-items-center ${news ? "mb-3" : ""}`}
-        >
-          <p className="subjects__text ">{desc}</p>
-          <Link target="_blank" to={link ?? ""}>
-            Acessar conteúdo
-          </Link>
+        <div className="subjects__desc">
+          <div
+            className={`flex flex-column  align-items-start ${news ? "mb-3" : ""}`}
+          >
+            <div className="flex justify-content-between align-items-end w-100">
+              <p className="subjects__text ">{desc}</p>
+              <Link className="subjects__link" target="_blank" to={link ?? ""}>
+                Acessar conteúdo
+              </Link>
+            </div>
+            {news && (
+              <p className="flex align-items-center gap-1 mt-2 subjects__news">
+                !!! Amigos, temos atualizações sobre essa matéria. Fiquem
+                atentos ao mural !!!
+              </p>
+            )}
+          </div>
         </div>
-        {news && (
-          <p className="flex align-items-center gap-1">
-            <i className="bx bx-alert-circle" /> Amigos, temos atualizações
-            sobre essa matéria. Fiquem atentos ao mural!
-          </p>
-        )}
       </div>
     </li>
   );
