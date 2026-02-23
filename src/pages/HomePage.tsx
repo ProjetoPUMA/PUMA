21b85824eb981a0e0f6657054a743b68048cc866
Tais Souza <tata.ssouz47@gmail.com>
2026-02-23 08:42:12 -0300

    style: tela mobile do mural concluída

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -267,7 +267,11 @@ function HomePage() {
           <div className="contact__header">
             <h4>Dúvidas? Sugestões? Quer ajudar de alguma forma?</h4>
             <h3>
-              <Link to="http://wa.me/5524988176141" target="_blank">
+              <Link
+                className="mr-4"
+                to="http://wa.me/5524988176141"
+                target="_blank"
+              >
                 Clique aqui
               </Link>
               e entre em contato agora!
